b6eda855ec97ce5b34fb182fdf04267c3e62fd97
eloiii <eloi.charra@gmail.com>
2023-05-25 16:37:34 +0100

    read port from command line and get expected ip

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import socket
+import struct
+import fcntl
+import sys
 from threading import *
 from enum import Enum
 from car_utilities.Led import *
@@ -29,7 +32,7 @@ class Server:
         - send power data
     """
 
-    def __init__(self, port=8888):
+    def __init__(self, port=8787):
         self.server = None
         self.start_tcp_server(port)
         self.motor_manager = Motor()
@@ -39,10 +42,13 @@ class Server:
         self.adc = Adc()
 
     def start_tcp_server(self, port):
-        hostname = socket.gethostname()
-        ip_addr = socket.gethostbyname(hostname)
-
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        ip_addr = socket.inet_ntoa(fcntl.ioctl(server.fileno(),
+                                               0x8915,
+                                               struct.pack('256s',b'wlan0'[:15])
+                                               )[20:24])
+
         server.bind((ip_addr, port))
         server.listen(1)
 
@@ -71,22 +77,23 @@ class Server:
     def treat_msg(self, msg):
         split_msg = msg.split(' ')
         cmd = split_msg[0]
-        if cmd == Command.CMD_MOTOR:
+
+        if cmd == Command.CMD_MOTOR.value:
             # motor 2000_2000_2000_2000
             self.activate_motor(split_msg[1])
-        elif cmd == Command.CMD_SERVO:
+        elif cmd == Command.CMD_SERVO.value:
             # servo 0_90
             self.activate_servo(split_msg[1])
-        elif cmd == Command.CMD_LED:
+        elif cmd == Command.CMD_LED.value:
             # led 0x01_255_255_255
             self.activate_led(split_msg[1])
-        elif cmd == Command.CMD_SONIC:
+        elif cmd == Command.CMD_SONIC.value:
             # sonic
             self.send_ultrasonic()
-        elif cmd == Command.CMD_BUZZER:
+        elif cmd == Command.CMD_BUZZER.value:
             # buzzer 1
             self.activate_buzzer(split_msg[1])
-        elif cmd == Command.CMD_LIGHT:
+        elif cmd == Command.CMD_LIGHT.value:
             # ??
             pass
         else:
@@ -106,7 +113,7 @@ class Server:
     def activate_led(self, param):
         # 0x01_255_255_255
         params = param.split('_')
-        int_converted = list(map(lambda n: int(n), params))
+        int_converted = list(map(lambda n: int(n, 0), params))
         self.led_manager.ledIndex(*int_converted)
 
     def send_ultrasonic(self, param):
@@ -114,8 +121,8 @@ class Server:
         pass
 
     def activate_buzzer(self, param):
-        self.buzzer_manager.run(int(param))
+        self.buzzer_manager.run(param)
 
 
 if __name__ == '__main__':
-    Server()
+    Server(int(sys.argv[1]))
