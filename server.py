7aa1272cbcfe0bdd63198fd4436328745a81042b
Lony Riffard <lony.riffard@etu.univ-grenoble-alpes.fr>
2023-05-26 12:51:11 +0200

    Debug

--- server.py
+++ server.py
@@ -109,9 +109,9 @@ class Server:
         self.buzzer_manager.run(param)
     
     def get_State(self):
-        CPUPercent = psutil.cpu_percent
-        MotorModel = Motor.getMotorModel #(FR/FL/BR/BL)
-        LedsBrightness = Led.ledsState
+        CPUPercent = psutil.cpu_percent()
+        MotorModel = Motor.getMotorModel() #(FR/FL/BR/BL)
+        LedsBrightness = Led.ledsState()
         SonicCount = self.sonic_count
         self.sonic_count = 0
         print(f'CPU :{CPUPercent}%\nWheels:\nFR:{MotorModel[0]}|FL:{MotorModel[1]}|BR:{MotorModel[2]}|BL:{MotorModel[3]}\nLedsBrightness:{LedsBrightness}\nSonicCount:{SonicCount}')
