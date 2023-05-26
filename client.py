4f21ab878c0dc2e7abd263eae775044e2956db67
eloiii <eloi.charra@gmail.com>
2023-05-26 10:52:38 +0100

    simple udp connection (client-side)

--- client.py
+++ client.py
@@ -6,9 +6,13 @@ from command import *
 
 class Client:
 
-    def __init__(self, ip, port=8888):
+    def __init__(self, ip, port=8787, video_port=8888):
         self.client = None
+        self.video_client = None
+        self.server_ip = ip
+        self.video_port = video_port
         self.start_tcp_client(ip, port)
+        self.start_udp_client()
 
     def start_tcp_client(self, ip, port):
 
@@ -18,12 +22,24 @@ class Client:
 
         self.client = client
 
-        thread = Thread(target=self.waiting_for_message)
+        # thread = Thread(target=self.waiting_for_message)
         # Thread test pour envoyer des messages
         # thread_send = Thread(target=self.send_msg)
 
-        thread.start()
+        # thread.start()
         # thread_send.start()
+
+    def start_udp_client(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client.makefile("rb")
+        self.video_client = client
+        self.request_video()
+
+    def request_video(self):
+        self.video_client.sendto('request_video'.encode(), (self.server_ip, self.video_port))
+        while True:
+            data, addr_serv = self.video_client.recvfrom(1024)
+            print(data)
 
     def waiting_for_message(self):
         while True:
@@ -50,4 +66,4 @@ class Client:
 
 
 if __name__ == '__main__':
-    client_ui = Client(sys.argv[1], int(sys.argv[2]))
+    client_ui = Client(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
