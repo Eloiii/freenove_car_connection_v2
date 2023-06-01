e46866b3b16ded8c820b6528b52a0d6af61e0e44
Lony Riffard <lony.riffard@etu.univ-grenoble-alpes.fr>
2023-06-01 14:30:20 +0100

    Premier tests

--- db/db.py
+++ db/db.py
@@ -1,6 +1,14 @@
 from owlready2 import *
 
+if __name__ == "__main__":
 
-onto = get_ontology("http://www.lesfleursdunormal.fr/static/_downloads/pizza_onto.owl")
-onto.load()
+    onto = get_ontology("file:///home/fenrir/INFO4/stage/4WD_Car_onto.owl").load()
+    for truc in onto.classes():
+        print(truc)
+    #default_world.set_backend(filename= "4WD_smart_car_db.sqlite3")
+    #default_world.save()
+    # Spécifier le fichier de base de données SQLite
+    # onto_path.append("4WD_smart_car_db.sqlite3")
 
+
+    
