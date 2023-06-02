190a42ec21d94e323d810522ea133221ddea919e
Lony Riffard <lony.riffard@etu.univ-grenoble-alpes.fr>
2023-06-02 13:32:15 +0100

    Database set up

--- db/db.py
+++ db/db.py
@@ -1,5 +1,6 @@
 from owlready2 import *
-from rdflib import Graph
+from tcp_connections.car_utilities.DataCollection import *
+
 
 def print_ontology(onto:Ontology):
     print("----------------Classes----------------")
@@ -15,24 +16,26 @@ def print_ontology(onto:Ontology):
         print(data)
     print("---------------------------------------")
     
-
+def add_car_data_to_db(data:Data, onto:Ontology):
+    #onto.search_one(is_a= onto.Car, MAC_Address=data.)
+    pass
 
 if __name__ == "__main__":
-
     default_world.set_backend(filename= "4WD_smart_car_db.sqlite3")
-    #onto = get_ontology("file://4WD_Smart_Car_onto.owl").load()
+    onto = default_world.get_ontology("http://www.semanticweb.org/fenrir/ontologies/2023/5/4WD-smart-car-ontology#").load()
+    #car = onto.search_one(is_a = onto.Car, IP_Address="127.0.0.1")
+    for individual in list(onto.individuals()): 
+        print(individual)
+        for prop in individual.get_properties():
+            for value in prop[individual]:
+                print("%s == %s" % (prop.python_name, value))
 
-    #test = onto.Car("Test")
-    #test.IP_Address.append("127.0.0.1")
-    #test.MAC_Address.append("00:1A:2B:3C:4D:ZZ")
-
-    for truc in default_world.individuals():
-        print(truc)
-        print(truc.IP_Address)
-        print(truc.MAC_Address)
     
     #default_world.save()
 
+#Du coup faire set_backend ça suffit ?? mettre des objets via ça c'est good ?
+#Est-ce que je dois modifier l'IRI ? Est-ce que je peu ?
+#Comment faire des requetes SPARQL ?
 
 ############################################################################
 #4WD_SMART_CAR_ONTOLOGY
