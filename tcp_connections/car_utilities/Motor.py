35d398b6f4dcaf08706b71684a8f9b8e39737de2
Eloi Charra <eloi.charra@gmail.com>
2023-06-09 11:57:42 +0100

    fix motors direction

--- tcp_connections/car_utilities/Motor.py
+++ tcp_connections/car_utilities/Motor.py
@@ -83,10 +83,10 @@ class Motor:
 
     def setMotorModel(self, duty1, duty2, duty3, duty4):
         duty1, duty2, duty3, duty4 = self.duty_range(duty1, duty2, duty3, duty4)
-        self.left_Upper_Wheel(duty1)
-        self.left_Lower_Wheel(duty2)
-        self.right_Upper_Wheel(duty3)
-        self.right_Lower_Wheel(duty4)
+        self.left_Upper_Wheel(-duty1)
+        self.left_Lower_Wheel(-duty2)
+        self.right_Upper_Wheel(-duty3)
+        self.right_Lower_Wheel(-duty4)
 
     def getMotorModel(self):
         return self.FrontRightWheelDuty, self.FrontLeftWheelDuty, self.BackRightWheelDuty, self.BackLeftWheelDuty
