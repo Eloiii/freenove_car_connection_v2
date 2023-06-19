3acce71638b1c4e46defe5642d45c38aaa3112e2
Eloi Charra <eloi.charra@gmail.com>
2023-06-19 15:24:32 +0100

    projecting 3D points of image (not working yet)

--- SLAM/ORB.py
+++ SLAM/ORB.py
@@ -1,6 +1,10 @@
-import numpy as np
-import cv2 as cv
 import os
+
+import cv2 as cv
+import numpy as np
+
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
 
 
 def draw_kp(img):
@@ -33,9 +37,9 @@ def find_matching():
     os.mkdir(f'./{orb_dir}')
 
     for k in range(n_files - 2):
-        print(f'images {k}.jpg and {k+1}.jpg')
+        print(f'images {k}.jpg and {k + 1}.jpg')
         img1 = cv.imread(f'../{images_dir}/{k}.jpg', cv.IMREAD_GRAYSCALE)
-        img2 = cv.imread(f'../{images_dir}/{k+1}.jpg', cv.IMREAD_GRAYSCALE)
+        img2 = cv.imread(f'../{images_dir}/{k + 1}.jpg', cv.IMREAD_GRAYSCALE)
 
         kp1, des1 = orb.detectAndCompute(img1, None)
         kp2, des2 = orb.detectAndCompute(img2, None)
@@ -46,12 +50,44 @@ def find_matching():
 
         img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:10], None,
                               flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        cv.imwrite(f'./{orb_dir}/{k}-{k+1}.jpg', img3)
+        cv.imwrite(f'./{orb_dir}/{k}-{k + 1}.jpg', img3)
+
+
+def extractRt(F):
+    W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
+    U, d, Vt = np.linalg.svd(F)
+    # assert np.linalg.det(U) > 0
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0
+    R = np.dot(np.dot(U, W), Vt)
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), Vt)
+    t = U[:, 2]
+    ret = np.eye(4)
+    ret[:3, :3] = R
+    ret[:3, 3] = t
+    # print(d)
+    return ret
+
+
+def triangulate(pose1, pose2, pts1, pts2):
+    ret = np.zeros((pts1.shape[0], 4))
+    pose1 = np.linalg.inv(pose1)
+    pose2 = np.linalg.inv(pose2)
+    for i, p in enumerate(zip(pts1, pts2)):
+        A = np.zeros((4, 4))
+        A[0] = p[0][0] * pose1[2] - pose1[0]
+        A[1] = p[0][1] * pose1[2] - pose1[1]
+        A[2] = p[1][0] * pose2[2] - pose2[0]
+        A[3] = p[1][1] * pose2[2] - pose2[1]
+        _, _, vt = np.linalg.svd(A)
+        ret[i] = vt[3]
+    return ret
 
 
 def slam():
     orb = cv.ORB_create()
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    bf = cv.BFMatcher(cv.NORM_HAMMING)
 
     img1 = cv.imread(f'../{images_dir}/0.jpg', cv.IMREAD_GRAYSCALE)
     pre_points, pre_features = orb.detectAndCompute(img1, None)
@@ -60,11 +96,64 @@ def slam():
     first_frame = img1
 
     map_initialised = False
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    while not map_initialised:
+    for k in range(10):
         curr_frame = cv.imread(f'../{images_dir}/{curr_frame_index}.jpg', cv.IMREAD_GRAYSCALE)
+        curr_points, curr_features = orb.detectAndCompute(curr_frame, None)
 
+        curr_frame_index += 1
 
+        matches = bf.knnMatch(pre_features, curr_features, k=2)
+
+        pre_matched_points = []
+        curr_matched_points = []
+        # ratio test as per Lowe's paper
+        for i, (m, n) in enumerate(matches):
+            if m.distance < 0.8 * n.distance:
+                curr_matched_points.append(curr_points[m.trainIdx].pt)
+                pre_matched_points.append(pre_points[m.queryIdx].pt)
+
+        pre_matched_points = np.array(pre_matched_points)
+        curr_matched_points = np.array(curr_matched_points)
+        # tform, inlier_tform_idx = cv.findHomography(pre_matched_points, curr_matched_points, method=cv.RANSAC)
+
+        F, mask = cv.findFundamentalMat(pre_matched_points, curr_matched_points, method=cv.FM_LMEDS)
+
+        inlier_pre_points = pre_matched_points[mask.ravel() == 1]
+        inlier_curr_points = curr_matched_points[mask.ravel() == 1]
+
+        E, _ = cv.findEssentialMat(inlier_pre_points, inlier_curr_points)
+
+        _, R, t, _ = cv.recoverPose(E, inlier_pre_points, inlier_curr_points)
+
+        K = np.matrix(
+            [[2.55072497e+03, 0.00000000e+00, 1.68091902e+03], [0.00000000e+00, 2.54174871e+03, 1.24377045e+03],
+             [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]).A
+        dist = np.array([[0.15913461, -0.06612768, -0.00569621, 0.01171116, -0.50291283]])
+
+        pre_pts_norm = cv.undistortPoints(inlier_pre_points, K, dist)
+        curr_pts_norm = cv.undistortPoints(inlier_curr_points, K, dist)
+
+        P1 = np.hstack((K, np.zeros((3, 1))))
+        P2 = np.hstack((np.dot(K, R), np.dot(K, t)))
+
+        points_4d_homogeneous = cv.triangulatePoints(P1, P2, pre_pts_norm, curr_pts_norm)
+
+        points_3d = cv.convertPointsFromHomogeneous(points_4d_homogeneous.T)
+
+        camera_location = -np.dot(R.T, t)
+
+        ax.scatter(points_3d[:, 0, 0], points_3d[:, 0, 1], points_3d[:, 0, 2], c='b', marker='o')
+        # ax.scatter3D(camera_location[0], camera_location[1], camera_location[2], c=camera_location[2], cmap='Blues')
+
+        # map_initialised = True
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -74,6 +163,6 @@ if __name__ == '__main__':
 
     # draw_kp()
 
-    find_matching()
+    # find_matching()
 
-
+    slam()
