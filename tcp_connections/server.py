939bbfef6a2316b68daf29d07f4be77a23418f95
Eloi Charra <eloi.charra@gmail.com>
2023-07-25 15:15:12 +0100

    modify imports

--- tcp_connections/server.py
+++ tcp_connections/server.py
@@ -12,12 +12,12 @@ from picamera2.encoders import MJPEGEncoder
 from picamera2.encoders import Quality
 from picamera2.outputs import FileOutput
 
-from tcp_connections.car_utilities.Buzzer import *
-from tcp_connections.car_utilities.DataCollection import *
-from tcp_connections.car_utilities.Led import *
-from tcp_connections.car_utilities.Light import *
-from tcp_connections.car_utilities.Ultrasonic import *
-from tcp_connections.command import *
+from car_utilities.Buzzer import *
+from car_utilities.DataCollection import *
+from car_utilities.Led import *
+from car_utilities.Light import *
+from car_utilities.Ultrasonic import *
+from command import *
 
 
 def get_mac_address():
